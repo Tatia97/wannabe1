88366aecaf93e86073943d7a0ce27586f2896b3d
tatia97 <Tsiskarauli84@gmail.com>
2022-01-15 12:54:31 +0400

    Final

--- index.js
+++ index.js
@@ -1,9 +1,37 @@
 let modal = document.getElementById("myModal");
-let view = document.getElementById("myView");
+let view = document.getElementsByClassName("view-p");
 let span = document.getElementsByClassName("close")[0];
+let btn = document.getElementById("myBtn")
 
-view.onclick = function() {
+view[0].onclick = function() {
     modal.style.display = "block";
+}
+view[1].onclick = function() {
+    modal.style.display = "block";
+}
+view[2].onclick = function() {
+    modal.style.display = "block";
+}
+view[3].onclick = function() {
+    modal.style.display = "block";
+}
+view[4].onclick = function() {
+    modal.style.display = "block";
+}
+view[5].onclick = function() {
+    modal.style.display = "block";
+}
+view[6].onclick = function() {
+    modal.style.display = "block";
+}
+view[7].onclick = function() {
+    modal.style.display = "block";
+}
+view[8].onclick = function() {
+    modal.style.display = "block";
+}
+btn.onclick = function() {
+    modal.style.display = "none";
 }
 
 span.onclick = function() {
